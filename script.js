b68180715c61b596506e5c3bae27afd1372f9bd2
Spenser Sagel <spenser.sagel@gmail.com>
2019-10-03 17:19:21 -0600

    HTMLframework

---
+++ script.js
@@ -0,0 +1,7 @@
+/* 
+    TODO:
+      -take in user input and format URL
+      -write multiple fetch calls to different weather api
+      -combine data into one set
+      -display data onto page
+*/
